1f4d639b08579fa1c57d93972dee94fd35720e5c
NikitaVr <nvarabei@gmail.com>
2020-05-01 02:13:15 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-27ad58407ab04b700629.js"
+    "url": "webpack-runtime-4f4f538931029e18b516.js"
   },
   {
     "url": "framework-8145bab88101e522accb.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1831d970eb382b6a1812967cd1c7516c"
+    "revision": "34d2f799bbdaefe560738303c34ba60c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-1cf1815cd9e7d53dcb02.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "0e2702e4e6c5df432105e7ae47fcd17b"
+    "revision": "17b86a2f21e3da0581e8d536c9eb1cee"
   },
   {
     "url": "manifest.webmanifest",
