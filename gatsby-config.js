ec16297b97bc0dbdcd9f6395a32316999f22ad6b
NikitaVr <nvarabei@gmail.com>
2020-05-01 12:28:17 -0400

    place google analytics in head

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-165269148-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
