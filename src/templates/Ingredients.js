1447c65e94230fe6b84bc0e5d30e6f2cb8eb6fd7
NikitaVr <nvarabei@gmail.com>
2020-04-30 20:38:51 -0400

    add search to ingredients and recipes

--- src/templates/Ingredients.js
+++ src/templates/Ingredients.js
@@ -1,19 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { convertToSlug } from "../utils/slug"
 import Layout from "./Layout/"
 import Article from "./Article/"
-import { ListGroup } from "react-bootstrap"
+import { ListGroup, InputGroup, FormControl } from "react-bootstrap"
+import { DebounceInput } from "react-debounce-input"
 
 const Ingredients = props => {
   const { pageContext } = props
   const { ingredients } = pageContext
+  const [search, setSearch] = useState()
+
+  const ingredientsF = ingredients.filter(
+    item =>
+      !search ||
+      (search && item.title.toLowerCase().includes(search.toLowerCase()))
+  )
   return (
     <Layout>
       <Article title="Ingredients">
         <div>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">🔍</InputGroup.Text>
+            </InputGroup.Prepend>
+            <DebounceInput
+              element={FormControl}
+              minLength={1}
+              debounceTimeout={300}
+              value={search}
+              onChange={e => setSearch(e.target.value)}
+            />
+          </InputGroup>
           <ListGroup>
-            {ingredients.map((item, index) => {
+            {ingredientsF.map((item, index) => {
               return (
                 <Link to={"/ingredients/" + convertToSlug(item.title)}>
                   <ListGroup.Item action>
--- src/templates/Recipes.js
+++ src/templates/Recipes.js
@@ -1,19 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { convertToSlug } from "../utils/slug"
 import Layout from "./Layout/"
 import Article from "./Article/"
-import { ListGroup } from "react-bootstrap"
+import { ListGroup, InputGroup, FormControl } from "react-bootstrap"
+import { DebounceInput } from "react-debounce-input"
 
 const Recipes = props => {
   const { pageContext } = props
   const { recipes } = pageContext
+  const [search, setSearch] = useState()
+
+  const recipesF = recipes.filter(
+    item =>
+      !search ||
+      (search && item.title.toLowerCase().includes(search.toLowerCase()))
+  )
   return (
     <Layout>
       <Article title="Recipes">
         <div>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">🔍</InputGroup.Text>
+            </InputGroup.Prepend>
+            <DebounceInput
+              element={FormControl}
+              minLength={1}
+              debounceTimeout={300}
+              value={search}
+              onChange={e => setSearch(e.target.value)}
+            />
+          </InputGroup>
           <ListGroup>
-            {recipes.map((item, index) => {
+            {recipesF.map((item, index) => {
               return (
                 <Link to={"/recipes/" + convertToSlug(item.title)}>
                   <ListGroup.Item action>
