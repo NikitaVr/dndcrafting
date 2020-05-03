a351571c4055bbddcd50c020a44a0ce19908ee9d
NikitaVr <nvarabei@gmail.com>
2020-05-03 11:04:51 -0400

    switch to using react-bootstrap navbar

--- src/templates/Layout/Nav.js
+++ src/templates/Layout/Nav.js
@@ -1,66 +1,45 @@
 import React from "react"
 import { Link } from "gatsby"
-const Nav = props => {
+import { Navbar, Nav, NavDropdown } from "react-bootstrap"
+const Navigation = props => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/">
-        DnD Crafting
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">DnD Crafting</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link>
             <Link className="nav-link" to="/locations">
               Locations
             </Link>
-          </li>
-          <li class="nav-item active">
+          </Nav.Link>
+          <Nav.Link>
             <Link className="nav-link" to="/recipes">
               Recipes
             </Link>
-          </li>
-          <li class="nav-item active">
+          </Nav.Link>
+          <Nav.Link>
             <Link className="nav-link" to="/ingredients">
               Ingredients
             </Link>
-          </li>
-          <li class="nav-item active">
-            <a
-              className="nav-link"
-              target="_blank"
-              href="https://github.com/NikitaVr/dndcrafting"
-            >
-              Contribute
-            </a>
-          </li>
-          <li class="nav-item active">
-            <a
-              className="nav-link"
-              target="_blank"
-              href="https://discord.gg/WUbvxZs"
-            >
-              Discord
-            </a>
-          </li>
-          <li class="nav-item active">
+          </Nav.Link>
+          <Nav.Link
+            href="https://github.com/NikitaVr/dndcrafting"
+            target="_blank"
+          >
+            Contribute
+          </Nav.Link>
+          <Nav.Link target="_blank" href="https://discord.gg/WUbvxZs">
+            Discord
+          </Nav.Link>
+          <Nav.Link>
             <Link className="nav-link" to="/">
               Team
             </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
-export default Nav
+export default Navigation
