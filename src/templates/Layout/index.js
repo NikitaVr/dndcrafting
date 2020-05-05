fa2397d6c9fcb242f64751dc1e09689f616f399a
NikitaVr <nvarabei@gmail.com>
2020-05-05 12:46:57 -0400

    fix background image

--- src/templates/Layout/index.js
+++ src/templates/Layout/index.js
@@ -2,21 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Nav from "./Nav"
+import "./layout.css"
 const Layout = props => {
   const { children } = props
   return (
-    <div
-      style={{
-        height: "100%",
-        width: "100%",
-        position: "absolute",
-        top: 0,
-        left: 0,
-        backgroundImage:
-          //"url(http://www.zingerbugimages.com/backgrounds/tan_parchment_paper_wallpaper_texture_seamless.jpg)",
-          "url(https://www.zingerbugimages.com/backgrounds/off_white_parchment_paper_wallpaper_texture_seamless.jpg)",
-      }}
-    >
+    <div>
       <Nav />
       {children}
     </div>
